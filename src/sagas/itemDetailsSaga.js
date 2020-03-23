54b6f1ab836db4eff076c10cda5b0c027d481295
codeinsack <codeinsack@gmail.com>
2020-03-23 10:37:42 +0300

    add fork

---
+++ src/sagas/itemDetailsSaga.js
@@ -0,0 +1,22 @@
+import { put, take, fork } from 'redux-saga/effects'
+import fetch from 'isomorphic-fetch'
+
+import {
+  SET_CART_ITEMS,
+  setItemDetails,
+} from '../actions'
+
+export function* loadItemDetails(item) {
+  console.log('item', item)
+  const { id } = item
+  const response = yield fetch(`http://localhost:8081/items/${id}`)
+  console.log('response item', response)
+  const data = yield response.json()
+  const info = data[0]
+  yield put(setItemDetails(info))
+}
+
+export function* itemDetailsSaga() {
+  const { items } = yield take(SET_CART_ITEMS)
+  yield items.map(item => fork(loadItemDetails, item))
+}
