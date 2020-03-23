9870978122776ce7ddff24261a39dee7132328af
codeinsack <codeinsack@gmail.com>
2020-03-23 10:52:38 +0300

    update fork logic

--- src/sagas/itemDetailsSaga.js
+++ src/sagas/itemDetailsSaga.js
@@ -7,16 +7,17 @@ import {
 } from '../actions'
 
 export function* loadItemDetails(item) {
-  console.log('item', item)
   const { id } = item
   const response = yield fetch(`http://localhost:8081/items/${id}`)
-  console.log('response item', response)
   const data = yield response.json()
   const info = data[0]
+  console.log('info', info)
   yield put(setItemDetails(info))
 }
 
 export function* itemDetailsSaga() {
   const { items } = yield take(SET_CART_ITEMS)
-  yield items.map(item => fork(loadItemDetails, item))
+  for (let i = 0; i < items.length; i++) {
+    yield fork(loadItemDetails, items[i])
+  }
 }
